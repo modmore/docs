253a17a9c9b54839e5e604baf8120297c7f5c699
Mark Hamstra <hello@markhamstra.com>
2016-09-10 01:29:16 +0200

    Add start to modmore daux.io theme/templates

---
+++ themes/modmore/templates/content.php
@@ -0,0 +1,25 @@
+<?php $this->layout('theme::layout/05_page') ?>
+
+<h1 class="Page__Header"><?= $page['breadcrumbs'] ? $this->get_breadcrumb_title($page, $base_page) : $page['title'] ?></h1>
+
+<div class="s-content">
+    <?= $page['content']; ?>
+</div>
+
+<?php if (!empty($page['prev']) || !empty($page['next'])) {
+?>
+<nav>
+    <ul class="Pager">
+        <?php if (!empty($page['prev'])) {
+    ?><li class=Pager--prev><a href="<?= $base_url . $page['prev']->getUrl() ?>">Previous</a></li><?php
+
+} ?>
+        <?php if (!empty($page['next'])) {
+    ?><li class=Pager--next><a href="<?= $base_url . $page['next']->getUrl() ?>">Next</a></li><?php
+
+} ?>
+    </ul>
+</nav>
+<?php
+
+} ?>
---
+++ themes/modmore/templates/layout/05_page.php
@@ -0,0 +1,86 @@
+<?php $this->layout('theme::layout/00_layout') ?>
+
+
+<a href="#sidebar" class="skiplink show-on-focus">Skip to sidebar navigation</a>
+<!--[if lte IE 9]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+
+<?php if ($params['html']['repo']) {
+    ?>
+    <a href="https://github.com/<?= $params['html']['repo']; ?>" target="_blank" id="github-ribbon" class="Github hidden-print"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+<?php
+
+} ?>
+
+
+<div class="pagewrapper">
+    <div id="pagecontent" class="pagecontent">
+        <header class="pageheader">
+            <form method="get" action="/search" class="docsearch">
+                <input type="search" id="tipue_search_input" name="query" placeholder="Search documentation...">
+                <button type="submit" class="button"><svg role="presentation"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="<?= $base_url; ?>themes/modmore/dist/sprite.svg#search" title="search"></use></svg></button>
+            </form>
+            <div class="headerlinks">
+                <a href="mailto:support@modmore.com">Contact Support</a>
+            </div>
+        </header>
+        <main class="maincontent" role="main" itemprop="mainContentOfPage">
+            <div class="row">
+                <div class="column">
+                    <div id="tipue_search_content"></div>
+
+                    <?= $this->section('content'); ?>
+                </div>
+            </div>
+        </main>
+    </div>
+    <aside id="sidebar" class="sidebar">
+        <p class="logo">
+            <strong>modmore docs</strong>
+        </p>
+
+        <nav class="pagenav">
+            <?php
+            $rendertree = $tree;
+            $path = '';
+
+            if ($page['language'] !== '') {
+                $rendertree = $tree->value[$page['language']];
+                $path = $page['language'];
+            }
+
+            echo $this->get_navigation($rendertree['en'], 'en', isset($params['request']) ? $params['request'] : '', $base_page, $params['mode']);
+            ?>
+
+        </nav>
+    </aside>
+</div>
+
+<!--
+<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script>
+    onContentLoaded(function() {
+        if (!window.jQuery) {
+            loadJS('/assets/templates/docs/src/bower_components/jquery/dist/jquery.min.js', false, function () {
+                loadJS('/assets/templates/docs/dist/main.js', true);
+            });
+        } else {
+            loadJS('/assets/templates/docs/dist/main.js', true);
+        }
+
+        // load image related scripts async
+        loadJS('/assets/templates/docs/dist/images.js', function(){
+            window.lazySizes.init();
+        });
+    });
+</script>
+-->
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -/->
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>-->
