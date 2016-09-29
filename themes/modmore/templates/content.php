6dcee2ef48b55e8fd0fe0a7b1d60fc997b65a635
Mark Hamstra <hello@markhamstra.com>
2016-09-29 12:06:01 +0200

    Be polite :D

--- themes/modmore/templates/content.php
+++ themes/modmore/templates/content.php
@@ -28,5 +28,5 @@
 <p><small>Found a typo, is a description unclear or missing completely?
         <a href="https://github.com/modmore/docs/edit/master/<?= $page['relative_path'] ?>" target="_blank">Edit this page</a>
         or
-        <a href="https://github.com/modmore/docs/issues/new?title=<?= urlencode($page['title']) ?>&body=On+page+[<?= urlencode($page['title']) ?>](https://github.com/modmore/docs/blob/master/<?= $page['relative_path'] ?>)+there+is+an+issue+with+..." target="_blank">report an issue</a>.
+        <a href="https://github.com/modmore/docs/issues/new?title=<?= urlencode($page['title']) ?>&body=On+page+[<?= urlencode($page['title']) ?>](https://github.com/modmore/docs/blob/master/<?= $page['relative_path'] ?>)+there+is+an+issue+with+..." target="_blank">report an issue</a>. Thank you!
     </small></p>
