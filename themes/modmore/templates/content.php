9b992b1cef8aafc74dba1f9e645c5f93bf2adb97
Mark Hamstra <hello@markhamstra.com>
2016-09-29 05:21:27 +0200

    Add edit and report an issue links

--- themes/modmore/templates/content.php
+++ themes/modmore/templates/content.php
@@ -23,3 +23,10 @@
 <?php
 
 } ?>
+
+
+<p><small>Found a typo, is a description unclear or missing completely?
+        <a href="https://github.com/modmore/docs/edit/master/<?= $page['relative_path'] ?>" target="_blank">Edit this page</a>
+        or
+        <a href="https://github.com/modmore/docs/issues/new?title=<?= urlencode($page['title']) ?>&body=On+page+[<?= urlencode($page['title']) ?>](https://github.com/modmore/docs/blob/master/<?= $page['relative_path'] ?>)+there+is+an+issue+with+..." target="_blank">report an issue</a>.
+    </small></p>
